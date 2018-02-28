bb51e03552f89be80f8c319f2208e60750582625
Brian Etzel <brianetzel@gmail.com>
2018-02-28 11:29:39 -0500

    Add generic connections file for local development with MySQL

---
+++ config/connections.js
@@ -0,0 +1,25 @@
+/**
+ * Connections
+ * (sails.config.connections)
+ */
+
+module.exports.connections = {
+
+  /***************************************************************************
+  *                                                                          *
+  * MySQL is the world's most popular relational database.                   *
+  * http://en.wikipedia.org/wiki/MySQL                                       *
+  *                                                                          *
+  * Run: npm install sails-mysql@for-sails-0.12 --save                       *
+  *                                                                          *
+  ***************************************************************************/
+  someMysqlServer: {
+    adapter: 'sails-mysql',
+    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_MYSQL_USER',
+    password: 'YOUR_MYSQL_PASSWORD',
+    database: 'YOUR_MYSQL_DB'
+  },
+
+
+};
