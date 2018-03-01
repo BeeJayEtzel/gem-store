28469fd1fee5d70a6f9ad7e7e0e59e7e088b07a5
Brian Etzel <brianetzel@gmail.com>
2018-02-28 22:39:10 -0500

    Add function to return store homepage

--- api/controllers/StoreController.js
+++ api/controllers/StoreController.js
@@ -6,6 +6,24 @@
  */
 
 module.exports = {
-	
+    /**
+     * storeHomepage
+     * 
+     * @description :: fetches a homepage for a given store
+     */
+    storeHomepage: function(req, res){
+        var s = req.params.storename;
+        Store.findOne({name: s}).exec(function(err, foundStore){
+            if (err) {
+                return req.view("500");
+            }
+            else if (foundStore !== null) {
+                return req.view("storehome", {store: foundStore});
+            }
+            else {
+                return req.view("404");
+            }
+        });
+    }
 };
 
