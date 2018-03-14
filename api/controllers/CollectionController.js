26ff6c2f53aa3b9e45316fe81a94a8bd0c893860
Brian Etzel <brianetzel@gmail.com>
2018-03-14 14:20:57 -0400

    Function to serve collection homepage

--- api/controllers/CollectionController.js
+++ api/controllers/CollectionController.js
@@ -11,18 +11,22 @@ module.exports = {
      * 
      * @description :: fetches a homepage for a given collection
      */
-    collectionHomepage: function(req, res){
-        Collection.find().exec(function(err, collectionFound){
-            if (err){
-                return res.view("500");
-            }
-            else if (collectionFound === null) {
-                return res.view("404");
-            } else {
-                return res.view("collection-homepage", {
-                    collections: collectionFound
-                });
-            }
+    collectionHome: function(req, res){
+        var c = req.params.collection
+        Collection.findOne({id: c})
+                  .populate('gems')
+                  .exec(function(err, collectionFound){
+                        if (err){
+                            return res.view("500");
+                        }
+                        else if (collectionFound === null) {
+                            return res.view("404");
+                        } else {
+                            return res.view("collection-homepage", {
+                                collection: collectionFound
+                            });
+                        }
+
         });
 
     },
