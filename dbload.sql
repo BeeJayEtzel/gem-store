ec1e29324a17a18393bbdf6056b1ecb3a5c4fe04
Brian Etzel <brianetzel@gmail.com>
2018-03-18 19:44:43 -0400

    Add file to load test data easily

---
+++ dbload.sql
@@ -0,0 +1,17 @@
+INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
+VALUES ("Ruby", "A fiery red gemstone", "/images/ruby.jpg", 299.99, 1, 3, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
+VALUES ("Diamond", "So shiny", "/images/diamond.jpg", 2989.99, 1, 4, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
+VALUES ("Amethyst", "Very much purple. Very", "/images/purple.jpg", 199.99, 1, 5, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
+VALUES ("Opal", "It's opal-y'", "/images/opal.jpg", 989.99, 1, 6, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
+VALUES ("Sapphire", "Shiny blue rock", "/images/diamond.jpg", 89.99, 1, 7, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
+VALUES ("Cubic Zirconia", "Not a diamond, but hey, you might fool her", "/images/diamond.jpg", 9.99, 1, 8, NOW(), NOW());
