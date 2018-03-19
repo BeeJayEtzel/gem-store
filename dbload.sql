f5d4af202bd159870d084c3e98151b840f668bf8
Brian Etzel <brianetzel@gmail.com>
2018-03-18 21:06:40 -0400

    Add/start file to load records into database

--- dbload.sql
+++ dbload.sql
@@ -1,17 +1,17 @@
-INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
-VALUES ("Ruby", "A fiery red gemstone", "/images/ruby.jpg", 299.99, 1, 3, NOW(), NOW());
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Ruby", "A fiery red gemstone", "/images/ruby.jpg", 2, 299.99, 1, 3, NOW(), NOW());
 
-INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
-VALUES ("Diamond", "So shiny", "/images/diamond.jpg", 2989.99, 1, 4, NOW(), NOW());
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Diamond", "So shiny", "/images/diamond.jpg", 2, 2989.99, 1, 4, NOW(), NOW());
 
-INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
-VALUES ("Amethyst", "Very much purple. Very", "/images/purple.jpg", 199.99, 1, 5, NOW(), NOW());
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Amethyst", "Very much purple. Very", "/images/purple.jpg", 2, 199.99, 1, 5, NOW(), NOW());
 
-INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
-VALUES ("Opal", "It's opal-y'", "/images/opal.jpg", 989.99, 1, 6, NOW(), NOW());
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Opal", "It's opal-y'", "/images/opal.jpg", 2, 989.99, 1, 6, NOW(), NOW());
 
-INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
-VALUES ("Sapphire", "Shiny blue rock", "/images/diamond.jpg", 89.99, 1, 7, NOW(), NOW());
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Sapphire", "Shiny blue rock", "/images/diamond.jpg", 2, 89.99, 1, 7, NOW(), NOW());
 
-INSERT INTO gem (name, description, picture, price, onhand, collection, id, createdAt, updatedAt)
-VALUES ("Cubic Zirconia", "Not a diamond, but hey, you might fool her", "/images/diamond.jpg", 9.99, 1, 8, NOW(), NOW());
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Cubic Zirconia", "Not a diamond, but hey, you might fool her", "/images/diamond.jpg", 2, 9.99, 1, 8, NOW(), NOW());
