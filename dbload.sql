15e582b79111e73d1d95f2bc2b051824eb92cc3e
Brian Etzel <brianetzel@gmail.com>
2018-03-21 19:23:52 -0400

    Update db script to add store & collections

--- dbload.sql
+++ dbload.sql
@@ -1,3 +1,18 @@
+INSERT INTO store (name, phone, email, id, createdAt, updatedAt)
+VALUES ("Gem Emporium", "555-555-5555", "email@gememporium.com", 1, NOW(), NOW())
+
+INSERT INTO collection (name, store, id, createdAt, updatedAt)
+VALUES ("Birth Stones", 1, 1, NOW(), NOW());
+
+INSERT INTO collection (name, store, id, createdAt, updatedAt)
+VALUES ("High Roller Gems", 1, 2, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Hope Diamond", "A glorius masterpiece", "/images/hopediamond.jpg", 1, 350000000, 2, 1, NOW(), NOW());
+
+INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
+VALUES ("Heart of the Ocean", "It'll never let go", "/images/blueheart.jpg", 1, 2200000, 2, 2, NOW(), NOW());
+
 INSERT INTO gem (name, description, picture, onhand, price, collection, id, createdAt, updatedAt)
 VALUES ("Ruby", "A fiery red gemstone", "/images/ruby.jpg", 2, 299.99, 1, 3, NOW(), NOW());
 
